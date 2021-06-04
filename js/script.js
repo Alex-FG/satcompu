8bd92ea2f8f073707322dfe716b6351375c55dca
AlexFG <alexfarinangog@gmail.com>
2021-06-04 02:17:56 -0500

    actualizacion pagina nosotros

--- js/script.js
+++ js/script.js
@@ -887,6 +887,7 @@
 							}
 						}
 
+
 						form.clearForm();
 
 						if (select.length) {
@@ -905,3 +906,185 @@
 		}
 	});
 }());
+
+/**nuevos scripts */
+/**funcionalidad botones de mision vision y politicas */
+$(document).ready(function(){
+	$('ul.tabs li a:first').addClass('actual');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.tabs li a').click(function(){
+		$('ul.tabs li a').removeClass('actual');
+		$(this).addClass('actual');
+		$('.secciones article').hide();
+
+		var actualTab = $(this).attr('href');
+		$(actualTab).show();
+		return false;
+	});
+});
+
+const slider = document.querySelector(".sliderc");
+const btnLeft = document.getElementById("moveLeft");
+const btnRight = document.getElementById("moveRight");
+const indicators = document.querySelectorAll(".indicator");
+
+
+let baseSliderWidth = slider.offsetWidth;
+let activeIndex = 0; // the current page on the slider
+
+/** <img src="images/partners/beaucoup.jpg" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt=""> */
+
+let movies = [
+  {
+    src: "images/partners/beaucoup.jpg",
+      href:"#",
+  },
+  {
+    src: "images/partners/dlink.jpg",
+      href:"#",
+  },
+
+  {
+    src: "images/partners/exfo.jpg",
+      href:"#",
+  },
+
+  {
+    src: "images/partners/fujikura.jpg",
+      href:"#",
+  },
+  {
+    src:"images/partners/mikrotik.png",
+      href:"#",
+  },
+  {
+    src: "images/partners/siemon.png",
+      href:"#",
+  },
+  
+    
+];
+
+
+
+
+// Fill the slider with all the movies in the "movies" array
+function populateSlider() {
+
+  movies.forEach((image) => {
+        // Clone the initial movie thats included in the html, then replace the image with a different one
+    const newMovie = document.getElementById("movie0");
+    let clone = newMovie.cloneNode(true);
+    let img = clone.querySelector("img");
+    let a = clone.querySelector("a");
+    img.src = image.src;
+    a.href = image.href;
+
+   
+    slider.insertBefore(clone, slider.childNodes[slider.childNodes.length - 1]);
+  });
+}
+
+/*
+function populatelink() {
+ movies.forEach((link) => {
+  const newlink = document.getElementById("movie0");
+  let clone = newlink.cloneNode(true);
+  let a = clone.querySelector("a");
+  a.href = link.href;
+});
+}*/
+
+
+
+populateSlider();
+populateSlider();
+
+
+
+// delete the initial movie in the html
+const initialMovie = document.getElementById("movie0");
+initialMovie.remove();
+
+// Update the indicators that show which page we're currently on
+function updateIndicators(index) {
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("active");
+  });
+  let newActiveIndicator = indicators[index];
+  newActiveIndicator.classList.add("active");
+}
+
+// Scroll Left button
+btnLeft.addEventListener("click", (e) => {
+  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
+    .width;
+  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
+
+  slider.scrollBy({
+    top: 0,
+    left: -scrollDistance,
+    behavior: "smooth",
+  });
+  activeIndex = (activeIndex - 1) % 3;
+  console.log(activeIndex);
+  updateIndicators(activeIndex);
+});
+
+function Derecha(){
+  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
+    .width;
+  let scrollDistance = movieWidth * 1; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
+
+  console.log(`movieWidth = ${movieWidth}`);
+  console.log(`scrolling right ${scrollDistance}`);
+
+  // if we're on the last page
+  if (activeIndex == 2) {
+    // duplicate all the items in the slider (this is how we make 'looping' slider)
+    populateSlider();
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = 0;
+    updateIndicators(activeIndex);
+  } else {
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = (activeIndex + 1) % 3;
+    console.log(activeIndex);
+    updateIndicators(activeIndex);
+  }}
+
+// Scroll Right button
+
+btnRight.addEventListener("click", (e) => {
+  Derecha()
+});
+
+
+//funcion para ejecutar auntomaticamente la funcion Derecha cada 5 seg
+setInterval(function(){
+  Derecha();
+},5000);
+
+// slider.addEventListener("scroll", (e) => {
+//   console.log(slider.scrollLeft);
+//   console.log(slider.offsetWidth);
+// });*/
+
+
+
+
